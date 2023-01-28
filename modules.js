b9a61187c6315eadd3d2c6f07cdff99a35154907
Atul1223 <a1atulk@gmail.com>
2023-01-28 13:55:41 +0530

    Learned modules/exports
    Signed-off-by: Atul1223 <a1atulk@gmail.com>

--- modules.js
+++ modules.js
@@ -1,3 +1,9 @@
-const xyz = require('./peopl')
-console.log(xyz)
+const xyz = require('./peopl');
+console.log(xyz.num);
+console.log(xyz.people);
+
+const {people} = require('./peopl');
+console.log(people);
+
+
 
--- peopl.js
+++ peopl.js
@@ -1,4 +1,11 @@
 const people = ['abc','bcd','xyz'];
-console.log(people);
+const num = [1,2,3,4,5];
+// console.log(people);
 
-module.exports = people
+//module.exports = people
+module.exports = {
+    people,num
+};
+
+const os = require('os');
+console.log(os.platform(),os.homedir());
